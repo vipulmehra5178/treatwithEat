5a75110b240c5a3533ced4b48daabde7f04e35a3
Vipul Mehra <vipulmehra02899@gmail.com>
2025-08-10 23:21:55 +0530

    update menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './components/navBar';
-import Footer from './components/footer';
-import Home from './pages/Home';
-import Menu from './pages/Menu'
-import About from './pages/about';
-import ReservationForm from './pages/reservationForm';
-import Availability from './pages/availability';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./components/navBar";
+import Footer from "./components/footer";
+import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/about";
+import ReservationForm from "./pages/reservationForm";
+import Availability from "./pages/availability";
 
 function App() {
   return (
@@ -14,9 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
+
         <Route path="/reservations" element={<ReservationForm />} />
         <Route path="/about" element={<About />} />
-        <Route path='/availability' element={<Availability />} />
+        <Route path="/availability" element={<Availability />} />
       </Routes>
       <Footer />
     </Router>

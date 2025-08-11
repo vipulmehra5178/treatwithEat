8fffab17100355dd6889b051c196234d9fa8a131
Vipul Mehra <vipulmehra02899@gmail.com>
2025-08-11 13:53:58 +0530

    Updated menu

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navBar";
 import Footer from "./components/footer";
 import Home from "./pages/Home";
-import Menu from "./pages/Menu";
+import Menu from './pages/Menu'
 import About from "./pages/about";
 import ReservationForm from "./pages/reservationForm";
 import Reservations from "./pages/Reservations";
@@ -12,7 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
+<Route path="/menu" element={<Menu/>}/>
         <Route path="/reservations" element={<Reservations />} />
         <Route path="/reservation-form" element={<ReservationForm />} />{" "}
         <Route path="/about" element={<About />} />
